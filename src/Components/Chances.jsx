6afe86f05c5c46f4ab101405f1da715cfbca81e4
Jeet252 <jeetnakrani2002@gmail.com>
2025-03-20 21:18:56 +0530

    Remove mistakenly pushed files

--- src/Components/Chances.jsx
+++ src/Components/Chances.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-function Chances() {
-  return <div></div>;
+function Chances({ word, setChancesData, chancesData, index }) {
+  const [inputValue, setInputValue] = useState([
+    { value: "", status: "" },
+    { value: "", status: "" },
+    { value: "", status: "" },
+    { value: "", status: "" },
+    { value: "", status: "" },
+  ]);
+  const reference = useRef([]);
+
+  const handleChange = (e) => {
+    let newinput = [...inputValue];
+    let id = parseInt(e.target.id);
+
+    newinput[id].value = e.target.value.toUpperCase();
+    setInputValue(newinput);
+    if (id < 4) {
+      reference.current[id + 1].focus();
+    }
+  };
+  const handleKeyPress = (e) => {
+    if (e.key == "Backspace") {
+      e.preventDefault();
+      let newInput = [...inputValue];
+      let id = parseInt(e.target.id);
+      newInput[id].value = "";
+      setInputValue([...newInput]);
+      if (id !== 0) {
+        setTimeout(() => {
+          reference.current[id - 1].focus();
+        }, 0);
+      }
+    }
+    if (e.key == "Enter") {
+      e.preventDefault();
+      if (!inputValue.some((value) => value.value == "")) {
+        for (let i = 0; i < 5; i++) {
+          let newInput = [...inputValue];
+          if (word.includes(inputValue[i].value)) {
+            if (word[i] == inputValue[i].value) {
+              newInput[i].status = "positionPresent";
+              setInputValue([...newInput]);
+            } else {
+              newInput[i].status = "present";
+              setInputValue([...newInput]);
+            }
+          } else {
+            newInput[i].status = "default";
+            setInputValue([...newInput]);
+          }
+        }
+        let chances = [...chancesData];
+        chances[index] = { ...chances[index], trialUsed: true };
+        chances[index + 1] = { ...chances[index + 1], trialUsed: false };
+        setChancesData(chances);
+      }
+    }
+  };
+  return (
+    <div className=" " onKeyDown={handleKeyPress}>
+      {inputValue.map((elem, elemIndex) => (
+        <input
+          key={elemIndex}
+          id={elemIndex}
+          ref={(el) => (reference.current[elemIndex] = el)}
+          className={`w-12 h-12 border border-gray-300 ${
+            elem.status == "default"
+              ? "bg-gray-500"
+              : elem.status == "present"
+              ? "bg-amber-500"
+              : elem.status == "positionPresent"
+              ? "bg-green-500"
+              : ""
+          } rounded text-center focus:outline-none focus:ring focus:border-blue-300`}
+          type="text"
+          maxLength={1}
+          value={elem.value}
+          onChange={handleChange}
+          autoComplete="off"
+          disabled={chancesData[index].trialUsed}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Chances;
--- src/Screens/GamePage.jsx
+++ src/Screens/GamePage.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Chances from "../Components/Chances";
+import axios from "axios";
 
 function GamePage() {
+  const [word, setWord] = useState();
+  // const wordExist = localStorage.getItem('word')
+  const [chancesData, setChancesData] = useState([
+    {
+      id: 1,
+      trialUsed: false,
+    },
+    {
+      id: 2,
+      trialUsed: true,
+    },
+    {
+      id: 3,
+      trialUsed: true,
+    },
+    {
+      id: 4,
+      trialUsed: true,
+    },
+    {
+      id: 5,
+      trialUsed: true,
+    },
+    {
+      id: 6,
+      trialUsed: true,
+    },
+  ]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          "https://api.frontendexpert.io/api/fe/wordle-words"
+        );
+        console.log(response.data);
+        console.log(Math.floor(Math.random() * (340 - 0 + 1)) + 0);
+      } catch (error) {}
+    })();
+  }, []);
+
   return (
     <>
-      <div>game page</div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+        <div className={`grid`}>
+          {chancesData.map((elem, index) => (
+            <Chances
+              key={elem.id}
+              index={index}
+              chancesData={chancesData}
+              word={word}
+              setChancesData={setChancesData}
+            />
+          ))}
+        </div>
+
+        <div className="flex space-x-2 mt-3">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 border border-gray-400 rounded flex items-center justify-center text-xl font-bold"
+            >
+              ?
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
